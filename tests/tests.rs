463767522f9c77a9cbb3a11bd1e64ce5b7ab5001
Sébastien GUERRI <13090870+sguerri@users.noreply.github.com>
2023-02-28 13:46:45 +0100

    Add tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -3,7 +3,6 @@ use colored_str::*;
 // cargo test -- --nocapture
 // cargo clippy -- -Dwarnings
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,6 +26,9 @@ mod tests {
         assert_eq!(colored("<unkwown>toto</>").to_string(), "<unkwown>toto</>");
         assert_eq!(colored("<on_unkwown>toto</>").to_string(), "<on_unkwown>toto</>");
 
+        assert_eq!(colored("<#FF000>toto</>").to_string(), "<#FF000>toto</>");
+        assert_eq!(colored("<on_#FF000>toto</>").to_string(), "<on_#FF000>toto</>");
+
         assert_eq!(colored("<+red>toto").to_string(), "<+red>toto");
         assert_eq!(colored("<+red>toto<->").to_string(), "<+red>toto<->");
         assert_eq!(colored("<+bold><->").to_string(), "<+bold><->");
@@ -43,6 +45,9 @@ mod tests {
         assert_eq!(colored("toto<red>toto</>").to_string(), "toto\x1B[31mtoto\x1B[0m");
         assert_eq!(colored("<red>toto</>toto").to_string(), "\x1B[31mtoto\x1B[0mtoto");
         assert_eq!(colored("toto<red>toto</>toto").to_string(), "toto\x1B[31mtoto\x1B[0mtoto");
+
+        assert_eq!(colored("<#FF0000>toto</>").to_string(), "\x1B[38;2;255;0;0mtoto\x1B[0m");
+        assert_eq!(colored("<on_#FF0000>toto</>").to_string(), "\x1B[48;2;255;0;0mtoto\x1B[0m");
 
         assert_eq!(colored("<red>toto</><blue>toto</>").to_string(), "\x1B[31mtoto\x1B[0m\x1B[34mtoto\x1B[0m");
         assert_eq!(colored("toto<red>toto</><blue>toto</>").to_string(), "toto\x1B[31mtoto\x1B[0m\x1B[34mtoto\x1B[0m");
@@ -75,56 +80,62 @@ mod tests {
 
         assert_eq!(colored("<red><+bold>toto<-></>").to_string(), "\x1B[1;31mtoto\x1B[0m");
         assert_eq!(colored("<red><+blue>toto<-></>").to_string(), "\x1B[34mtoto\x1B[0m");
+        assert_eq!(colored("<red><+bold+on_blue>toto<-></>").to_string(), "\x1B[1;44;31mtoto\x1B[0m");
 
         assert_eq!(colored("<red>toto<+bold>toto<-></>").to_string(), "\x1B[31mtoto\x1B[0m\x1B[1;31mtoto\x1B[0m");
         assert_eq!(colored("<red><+bold>toto<->toto</>").to_string(), "\x1B[1;31mtoto\x1B[0m\x1B[31mtoto\x1B[0m");
         assert_eq!(colored("<red>toto<+bold>toto<->toto</>").to_string(), "\x1B[31mtoto\x1B[0m\x1B[1;31mtoto\x1B[0m\x1B[31mtoto\x1B[0m");
+        assert_eq!(colored("toto<red>toto<+bold>toto<->toto</>").to_string(), "toto\x1B[31mtoto\x1B[0m\x1B[1;31mtoto\x1B[0m\x1B[31mtoto\x1B[0m");
+        assert_eq!(colored("<red>toto<+bold>toto<->toto</>toto").to_string(), "\x1B[31mtoto\x1B[0m\x1B[1;31mtoto\x1B[0m\x1B[31mtoto\x1B[0mtoto");
+        assert_eq!(colored("toto<red>toto<+bold>toto<->toto</>toto").to_string(), "toto\x1B[31mtoto\x1B[0m\x1B[1;31mtoto\x1B[0m\x1B[31mtoto\x1B[0mtoto");
 
-        assert_eq!(colored("<red><+bold+on_blue>toto<-></>").to_string(), "\x1B[1;44;31mtoto\x1B[0m");
+        assert_eq!(colored("<red>toto<+bold>toto<-><+italic>toto<->toto</>").to_string(), "\x1B[31mtoto\x1B[0m\x1B[1;31mtoto\x1B[0m\x1B[3;31mtoto\x1B[0m\x1B[31mtoto\x1B[0m");
+        assert_eq!(colored("<red>toto<+bold>toto<->toto<+italic>toto<->toto</>").to_string(), "\x1B[31mtoto\x1B[0m\x1B[1;31mtoto\x1B[0m\x1B[31mtoto\x1B[0m\x1B[3;31mtoto\x1B[0m\x1B[31mtoto\x1B[0m");
 
-
-        // assert_eq!(colored("<red>toto<+bold>toto<->toto</>").to_string(), "\x1B[31mtoto\x1B[0m\x1B[1;31mtoto\x1B[0m\x1B[31mtoto\x1B[0m");
+        assert_eq!(colored("<red><+bold>toto\ntoto<-></>").to_string(), "\x1B[1;31mtoto\ntoto\x1B[0m");
     }
 
     #[test]
     fn str_trait()
     {
+        assert_eq!("<red></>".colored().to_string(), "");
+        assert_eq!("<red>toto</>".colored().to_string(), "\x1B[31mtoto\x1B[0m");
+        assert_eq!("<#FF0000>toto</>".colored().to_string(), "\x1B[38;2;255;0;0mtoto\x1B[0m");
+        assert_eq!("<on_#FF0000>toto</>".colored().to_string(), "\x1B[48;2;255;0;0mtoto\x1B[0m");
+        assert_eq!("<red>toto</><blue>toto</>".colored().to_string(), "\x1B[31mtoto\x1B[0m\x1B[34mtoto\x1B[0m");
+        assert_eq!("<red+bold>toto</>".colored().to_string(), "\x1B[1;31mtoto\x1B[0m");
+        assert_eq!("<red+blue>toto</>".colored().to_string(), "\x1B[34mtoto\x1B[0m");
+        assert_eq!("<bold+bold>toto</>".colored().to_string(), "\x1B[1mtoto\x1B[0m");
+        assert_eq!("<red>toto\ntoto</>".colored().to_string(), "\x1B[31mtoto\ntoto\x1B[0m");
 
+        assert_eq!("<red><+bold><-></>".colored().to_string(), "");
+        assert_eq!("<red><+bold>toto<-></>".colored().to_string(), "\x1B[1;31mtoto\x1B[0m");
+        assert_eq!("<red>toto<+bold>toto<-></>".colored().to_string(), "\x1B[31mtoto\x1B[0m\x1B[1;31mtoto\x1B[0m");
+        assert_eq!("<red>toto<+bold>toto<-><+italic>toto<->toto</>".colored().to_string(), "\x1B[31mtoto\x1B[0m\x1B[1;31mtoto\x1B[0m\x1B[3;31mtoto\x1B[0m\x1B[31mtoto\x1B[0m");
+        assert_eq!("<red><+bold>toto\ntoto<-></>".colored().to_string(), "\x1B[1;31mtoto\ntoto\x1B[0m");
     }
 
     #[test]
     fn str_macro()
     {
+        let toto = "toto";
 
+        assert_eq!(cformat!(), "");
+        assert_eq!(cformat!("<red>{toto}</>"), "\x1B[31mtoto\x1B[0m");
+        assert_eq!(cformat!("<red>{}</>", toto), "\x1B[31mtoto\x1B[0m");
+
+        assert_eq!(cformat!("<#FF0000>{toto}</>"), "\x1B[38;2;255;0;0mtoto\x1B[0m");
+        assert_eq!(cformat!("<on_#FF0000>{toto}</>"), "\x1B[48;2;255;0;0mtoto\x1B[0m");
+        assert_eq!(cformat!("<red>{toto}</><blue>{toto}</>"), "\x1B[31mtoto\x1B[0m\x1B[34mtoto\x1B[0m");
+        assert_eq!(cformat!("<red+bold>{toto}</>"), "\x1B[1;31mtoto\x1B[0m");
+        assert_eq!(cformat!("<red+blue>{toto}</>"), "\x1B[34mtoto\x1B[0m");
+        assert_eq!(cformat!("<bold+bold>{toto}</>"), "\x1B[1mtoto\x1B[0m");
+        assert_eq!(cformat!("<red>{toto}\n{toto}</>"), "\x1B[31mtoto\ntoto\x1B[0m");
+
+        assert_eq!(cformat!("<red><+bold>{toto}<-></>"), "\x1B[1;31mtoto\x1B[0m");
+        assert_eq!(cformat!("<red>{toto}<+bold>{toto}<-></>"), "\x1B[31mtoto\x1B[0m\x1B[1;31mtoto\x1B[0m");
+        assert_eq!(cformat!("<red>{toto}<+bold>{toto}<-><+italic>{toto}<->{toto}</>"), "\x1B[31mtoto\x1B[0m\x1B[1;31mtoto\x1B[0m\x1B[3;31mtoto\x1B[0m\x1B[31mtoto\x1B[0m");
+        assert_eq!(cformat!("<red><+bold>{toto}\n{toto}<-></>"), "\x1B[1;31mtoto\ntoto\x1B[0m");
     }
 
-
-    #[test]
-    fn it_works() {
-
-        let blue = "\x1B[34m";
-        let reset = "\x1B[0m";
-        assert_eq!(cformat!("<blue>toto</>"), format!("{blue}toto{reset}"));
-
-
-
-
-        // let toto = "toto";
-        // println!("{}", toto.red());
-        // assert!(false)
-        // assert_eq!(cformat!(), "");
-        // assert_eq!(cformat!("<red>toto</>"), "\u{1b}[31mtoto\u{1b}[0m");
-        // dbg!(cformat!("<red>{}</>", "content"));
-        // coloredln!("{}", "toto");
-        // coloredln!("{}", "toto <red>red</>");
-        // coloredln!("first <red>red <+on_blue>on_blue<-> red</> last");
-        // coloredln!("first <red>red <+on_blue>on_blue<-> red</> middle <yellow>yellow</> last");
-        // coloredln!("first <red>red <+on_blue>on_blue<-> red</> middle <yellow>yellow</> last");
-        // coloredln!("first <red>red <+on_blue>on_blue<-> between <+on_yellow>on_yellow<-> red</> last");
-        // coloredln!("first <red>red <+on_blue>on_blue<-> between <+on_yellow>on_yellow<-> between again <+on_green>on_green<-> red</> last");
-        // println!("{}", " dsq <red>red <+on_blue>blue<-> red</> dsq".colored());
-        // println!("{}", "{red}red {+bold}bold{-} none {+italic}italic{-} red{/}".colored());
-        // let result = add(2, 2);
-        // assert_eq!(result, 4);
-    }
 }
