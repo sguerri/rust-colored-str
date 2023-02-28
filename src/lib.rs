903026db89d72baac60b32fe545fb3ad3b3e3cd1
Sébastien GUERRI <13090870+sguerri@users.noreply.github.com>
2023-02-28 21:42:25 +0100

    Update doc and readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+// Copyright (C) 2023 Sebastien Guerri
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #![warn(missing_docs)]
 
 //! Coloring terminal by parsing string content
@@ -13,14 +28,16 @@
 //! 
 //! ```toml
 //! [dependencies]
-//! colored-str = "1"
+//! colored-str = "0.0.0"
 //! ```
 //! 
-//! ## Features
+//! ## How to use
 //! 
 //! Styles must be written within `<...>` opening flag and `</>` closing flag.
 //! 
 //! Style variations must be written within `<+...>` opening flag and `<->` closing flag.
+//! 
+//! See below examples.
 //! 
 //! ## Limitations
 //! 
